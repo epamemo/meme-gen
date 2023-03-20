04968f2e0760003afd98b4b8e187f62ffae50b5f
Epafraditus Memoriano <epamemo@gmail.com>
2023-03-20 17:30:43 +0700

    update meme-gen able to select a template and change the api call

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const generateBtn = document.getElementById("generate-btn");
+const templateSelect = document.getElementById("template-select");
 const topText = document.getElementById("top-text");
 const bottomText = document.getElementById("bottom-text");
+const generateBtn = document.getElementById("generate-btn");
 const memeContainer = document.querySelector(".meme-container");
 const memeImg = memeContainer.querySelector("img");
 
+fetch("https://api.memegen.link/templates")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(template => {
+      const option = document.createElement("option");
+      option.value = template.id;
+      option.textContent = template.name;
+      templateSelect.appendChild(option);
+    });
+  })
+  .catch(error => console.error(error));
+
 generateBtn.addEventListener("click", function () {
-  memeImg.src = `https://api.memegen.link/images/custom/${encodeURIComponent(
-    topText.value
-  )}/${encodeURIComponent(bottomText.value)}.png`;
+  const selectedTemplateId = templateSelect.value;
+  const memeUrl = `https://api.memegen.link/images/${selectedTemplateId}/${encodeURIComponent(topText.value)}/${encodeURIComponent(bottomText.value)}.png`;
+  memeImg.src = memeUrl;
 });
